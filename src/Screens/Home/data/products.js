78ca4e8fb6103875f123823a900bae67e32eef91
3abdawy <elabd1996@gmail.com>
2024-03-31 02:58:16 +0200

    new data structure

---
+++ src/Screens/Home/data/products.js
@@ -0,0 +1,8 @@
+export const products = [
+  {id: 'p1', title: 'iPhone 12', categoryId: '2'},
+  {id: 'p2', title: 'Samsung Galaxy S21', categoryId: '2'},
+  {id: 'p3', title: 'MacBook Air', categoryId: '3'},
+  {id: 'p4', title: 'Dell XPS 13', categoryId: '3'},
+  {id: 'p5', title: 'LG Washing Machine', categoryId: '5'},
+  {id: 'p6', title: 'Samsung Refrigerator', categoryId: '6'},
+];
