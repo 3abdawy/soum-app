77d9a915e852f9885ce50ba40ce1708e50478114
3abdawy <elabd1996@gmail.com>
2024-03-31 16:30:20 +0200

    Fix products data

--- src/Screens/Home/data/products.js
+++ src/Screens/Home/data/products.js
@@ -6,7 +6,7 @@ export const products = [
   {id: 'p3', title: 'Samsung Galaxy', categoryId: '13'},
   {id: 'p4', title: 'Samsung Note', categoryId: '14'},
   {id: 'p5', title: 'MacBook Air', categoryId: '8'},
-  {id: 'p6', title: 'Dell XPS 13', categoryId: '3'},
+  {id: 'p6', title: 'Dell XPS 13', categoryId: '7'},
   {id: 'p7', title: 'LG Washing Machine', categoryId: '5'},
   {id: 'p8', title: 'Samsung Refrigerator', categoryId: '6'},
 ];
